b1a6447873ae946ab04719a9c14ddc7cddb8e8d1
drsi <a.shteyman@rsoperation.com>
2022-07-10 20:37:58 +0300

    ready

--- SKILL_BOX/practice_10.1-10.5.py
+++ SKILL_BOX/practice_10.1-10.5.py
@@ -50,3 +50,48 @@
 #         else:
 #             print(' ', end='')
 #     print()
+
+# 10.3.1
+
+#
+# for row in range(20):
+#     for col in range(30):
+#         if row == 0:
+#             print('-', end='')
+#         elif col == 0:
+#             print('|', end='')
+#         elif col == 29:
+#             print('|', end='')
+#         else:
+#             print(' ', end='')
+#     print()
+
+# 10.3.2
+#дорога (диагональная)
+
+# for row in range(20):
+#     for col in range(50):
+#         if row == 9:
+#             print('-', end='')
+#         elif col == row + 29:
+#             print('\\', end='')
+#         elif col == -row  + 19:
+#             print('/', end='')
+#         elif col == 24:
+#             print('|', end='')
+#         else:
+#             print(' ', end='')
+#     print()
+
+# 10.3.3
+
+size = int(input('Введите размер матрицы: '))
+for row in range (size):
+    for col in range (size):
+        if row + col == size -1:
+            print(1, end= ' ')
+        elif row >= size - col:
+            print(2, end=' ')
+        else:
+            print(0, end =' ')
+    print()
