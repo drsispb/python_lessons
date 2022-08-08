341139989aa2dc62d303af5caf261510ef186caf
drsi <a.shteyman@rsoperation.com>
2022-08-08 23:05:06 +0300

    upgrade: fix bag

--- skill_box_12/12.8.py
+++ skill_box_12/12.8.py
@@ -9,28 +9,20 @@ input содержит корректное приглашение для вво
 вывод содержит описание результата (выведенные числа сопровождаются текстовым описанием).
 '''
 
-def search_max_del():
+def search_max_del(firth, second):
     x = 0
     if firth > second:
-        for i in range(1, firth + 1):
-            if firth % i == 0 and second % i == 0:
-                x = i
-            else:
-                continue
-        print(x)
+        i = second
     else:
-        for i in range(1, second + 1):
-            if firth % i == 0 and second % i == 0:
-                x = i
-            else:
-                continue
-        print(x)
-
-
+        i = firth
+    for num in range(1, i + 1):
+        if firth % num == 0 and second % num == 0:
+            x = num
+    print('Наибольший общий делитель:', x)
 
 
 firth = int(input('Введите первое число: '))
 second = int(input('Введите второе число: '))
-search_max_del()
+search_max_del(firth, second)
 
 
