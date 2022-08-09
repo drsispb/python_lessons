2f8cfd253dfcd94f76d9fa44f1a78f32d1102db9
drsi <a.shteyman@rsoperation.com>
2022-08-09 20:59:16 +0300

    finish

--- skill_box_12/12.8.py
+++ skill_box_12/12.8.py
@@ -9,20 +9,34 @@ input содержит корректное приглашение для вво
 вывод содержит описание результата (выведенные числа сопровождаются текстовым описанием).
 '''
 
-def search_max_del(firth, second):
-    x = 0
-    if firth > second:
-        i = second
-    else:
-        i = firth
-    for num in range(1, i + 1):
-        if firth % num == 0 and second % num == 0:
-            x = num
-    print('Наибольший общий делитель:', x)
+# def search_max_del(firth, second):
+#     x = 0
+#     if firth > second:
+#         i = second
+#     else:
+#         i = firth
+#     for num in range(1, i + 1):
+#         if firth % num == 0 and second % num == 0:
+#             x = num
+#     print('Наибольший общий делитель:', x)
+#
+#
+# firth = int(input('Введите первое число: '))
+# second = int(input('Введите второе число: '))
+# search_max_del(firth, second)
+
+'''"типо правильный вариант'''
+
+def search_evklid(a, b):
+    while a != 0 and b != 0:
+        if a > b:
+           a = a % b
+        else:
+            b = b % a
+    print(a + b)
 
 
 firth = int(input('Введите первое число: '))
 second = int(input('Введите второе число: '))
-search_max_del(firth, second)
 
-
+search_evklid(firth, second)
