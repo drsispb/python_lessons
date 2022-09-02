1207e9f46ec889ec18fc857437c1356e57294f91
drsispb <100297855+drsispb@users.noreply.github.com>
2022-09-02 14:15:18 +0300

    Skill box 17 (#12)
    * выполнено
    
    * выполнено
    
    * понял, что неправильно сделал 17.10 и исправлял
    
    * исправлено согласно комментариям

---
+++ skill_box_17_List comprehensions/17.10.py
@@ -0,0 +1,33 @@
+'''Задача 10. Шифр Цезаря
+Что нужно сделать
+
+Юлий Цезарь использовал свой способ шифрования текста. Каждая буква заменялась на следующую по алфавиту через K позиций
+по кругу. Если взять русский алфавит и K = 3, то в слове, которое мы хотим зашифровать,
+буква А станет буквой Г, Б станет Д и так далее.
+Пользователь вводит сообщение, а также значение сдвига. Напишите программу, которая зашифрует это сообщение
+при помощи шифра Цезаря.
+
+Пример:
+Введите сообщение: это питон.
+Введите сдвиг: 3
+Зашифрованное сообщение: ахс тлхср.'''
+
+alfabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+space = ' '
+
+message = input('Введите сообщение: ').lower()
+step = int(input('Введите сдвиг: '))
+schifer = [alfabet[(alfabet.index(i) + step) % len(alfabet)] if i in alfabet else i for i in message]
+# for i in message:
+#     place = alfabet.find(i)
+#     new_place = place + step
+#     if i in alfabet and new_place < (len(alfabet) - 1):
+#         schifer += alfabet[new_place]
+#     elif i == space:
+#         schifer += space
+#     elif i in alfabet and new_place >= (len(alfabet) -1):
+#         schifer += alfabet[new_place - len(alfabet)]
+#     else:
+#         schifer += i
+
+print('Зашифрованное сообщение: ', ''.join(schifer))
