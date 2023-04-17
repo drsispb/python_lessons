2970433dbf30602e88ea6170e62b173126fba280
drsispb <100297855+drsispb@users.noreply.github.com>
2023-04-17 20:43:48 +0300

    выполнено (#22)
    * выполнено
    
    * 25 Основные принципы ООП
    1. исправлен
    2. исправлен
    5. Исправлен
    
    * переписал дочерние классы

---
+++ 25.Основные принципы ООП/25.1.py
@@ -0,0 +1,70 @@
+'''Задача 1. Налоги
+Что нужно сделать
+Реализуйте иерархию классов, описывающих имущество налогоплательщиков. Она должна состоять из базового класса Property
+и производных от него классов Apartment, Car и CountryHouse.
+
+Базовый класс должен иметь атрибут worth (стоимость), который передаётся в конструктор, и метод расчёта налога,
+ переопределённый в каждом из производных классов. Налог на квартиру вычисляется как 1/1000 её стоимости, на машину — 1/200, на дачу — 1/500.
+
+Каждый дочерний класс должен иметь конструктор с одним параметром, передающий свой параметр конструктору базового класса.
+
+Разработайте интерфейс программы. Пусть она запрашивает у пользователя количество его денег и стоимость имущества,
+а затем выдаёт налог на соответствующее имущество и показывает, сколько денег ему не хватает (если это так).
+'''
+
+class Property:
+
+    ''' Базовый класс, для приема в себя стиомости '''
+    def __init__(self, worth, rate=1):
+        self.worth = worth
+        self.rate = rate
+
+    def __str__(self):
+        return '{} стоят {}:  \nРазмер налога {}: \n'
+
+    def tax_calculation(self):
+        '''Метод для расчета налога'''
+        return self.worth * (1 / self.rate)
+
+
+class Apartment(Property):
+    '''Дочерний класс для апартаментов, принимает стоимость и налоговую базу'''
+    rate = 1000
+    def __init__(self, worth):
+        super().__init__(worth)
+
+    def __str__(self):
+        return super().__str__().format('Апартаменты', self.worth, self.tax_calculation())
+
+class Car(Property):
+    '''Дочерний класс для автомобиля, принимает стоимость и налоговую базу'''
+    rate = 200
+    def __init__(self, worth):
+        super().__init__(worth)
+
+    def __str__(self):
+        return super().__str__().format('Машина', self.worth, self.tax_calculation())
+
+
+class CountryHouse(Property):
+    '''Дочерний класс для загородного дома, принимает стоимость и налоговую базу'''
+    rate = 500
+    def __init__(self, worth):
+        super().__init__(worth)
+
+    def __str__(self):
+        return super().__str__().format('Загородный дом', self.worth, self.tax_calculation())
+
+aptWorth = int(input('Введите стоимость квартиры: '))
+apt = Apartment(worth=aptWorth)
+carWorth = int(input('Введите стоимость автомобиля: '))
+car = Car(worth=carWorth)
+cHWorth = int(input('Введите стоимость загородного дома: '))
+cHouse = CountryHouse(worth=cHWorth)
+
+all_tax = apt.tax_calculation() + car.tax_calculation() + cHouse.tax_calculation()
+money = int(input('Введите сколько у вас имеется денег '))
+if all_tax > money:
+    print('Очень жаль, вам не хватет денег на оплату налогов ;(   в размере', all_tax - money)
+else:
+    print('Поздравляю, ваших денег хватит на оплату налогов')
---
+++ 25.Основные принципы ООП/25.2.py
@@ -0,0 +1,70 @@
+'''Задача 2. Карма
+Что нужно сделать
+Один буддист-программист решил создать свой симулятор жизни, в котором нужно набрать 500 очков кармы
+(это константа), чтобы достичь просветления.
+
+Каждый день вызывается специальная функция one_day(), которая возвращает количество кармы от 1 до 7 и
+может с вероятностью 1 к 10 выкинуть одно из исключений:
+
+KillError,
+DrunkError,
+CarCrashError,
+GluttonyError,
+DepressionError.
+(Исключения нужно создать самостоятельно, при помощи наследования от Exception.)
+Напишите такую программу. Функцию оберните в бесконечный цикл, выход из которого возможен только при накоплении
+кармы до уровня константы. Исключения обработайте и запишите в отдельный лог karma.log.
+По итогу у вас может быть примерно такая структура программы:
+открываем файл
+цикл по набору кармы
+   try
+      карма += one_day()
+   except(ы) с указанием классов исключений, которые нужно поймать
+      добавляем запись в файл
+закрываем файл'''
+
+import random
+
+
+'''Прочие классы, для инициализации информации об исключении'''
+class KillError(Exception):
+    def __str__(self):
+        return 'KillError'
+class DrunkError(Exception):
+    def __str__(self):
+        return 'DrunkError'
+class CarCrashError(Exception):
+    def __str__(self):
+        return 'CarCrashError'
+class GluttonyError(Exception):
+    def __str__(self):
+        return 'GluttonyError'
+class DepressionError(Exception):
+    def __str__(self):
+        return 'DepressionError'
+
+
+
+def one_day():
+    '''функция для получения кармы, либо инициализации исключения'''
+    try:
+        if random.randint(1,10) == 2:
+            raise
+        else:
+            return random.randint(1,7)
+    except:
+        error = random.choice([KillError(), CarCrashError(), DrunkError(), GluttonyError(), DepressionError()])
+        file.write(str(error) + '\n')
+        return 0
+
+with open('karma.log', 'w', encoding='utf8') as file:
+
+    constanta = 500
+    carma = 0
+    cnt = 0
+
+    while carma <= constanta:
+        cnt += 1
+        carma += one_day()
+
+    print('Уровень кармы достигнут, потребовалось ', cnt, 'дня(-ей)')
