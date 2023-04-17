10bdd7e85e042e8067ff7ad398d17006543e4235
Питер Паркер <a.shteyman@rsoperation.com>
2023-04-17 20:42:06 +0300

    переписал дочерние классы

--- 25.Основные принципы ООП/25.1.py
+++ 25.Основные принципы ООП/25.1.py
@@ -29,20 +29,18 @@ class Property:
 
 class Apartment(Property):
     '''Дочерний класс для апартаментов, принимает стоимость и налоговую базу'''
-
+    rate = 1000
     def __init__(self, worth):
         super().__init__(worth)
-        super().__init__(1000)
-
 
     def __str__(self):
         return super().__str__().format('Апартаменты', self.worth, self.tax_calculation())
 
 class Car(Property):
     '''Дочерний класс для автомобиля, принимает стоимость и налоговую базу'''
+    rate = 200
     def __init__(self, worth):
         super().__init__(worth)
-        super().__init__(200)
 
     def __str__(self):
         return super().__str__().format('Машина', self.worth, self.tax_calculation())
@@ -50,9 +48,9 @@ class Car(Property):
 
 class CountryHouse(Property):
     '''Дочерний класс для загородного дома, принимает стоимость и налоговую базу'''
+    rate = 500
     def __init__(self, worth):
         super().__init__(worth)
-        super().__init__(500)
 
     def __str__(self):
         return super().__str__().format('Загородный дом', self.worth, self.tax_calculation())
@@ -63,9 +61,7 @@ carWorth = int(input('Введите стоимость автомобиля: ')
 car = Car(worth=carWorth)
 cHWorth = int(input('Введите стоимость загородного дома: '))
 cHouse = CountryHouse(worth=cHWorth)
-print(apt)
-print(car)
-print(cHouse)
+
 all_tax = apt.tax_calculation() + car.tax_calculation() + cHouse.tax_calculation()
 money = int(input('Введите сколько у вас имеется денег '))
 if all_tax > money:
