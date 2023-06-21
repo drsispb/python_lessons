2f3fb1d368cdf5b8908638a74f35034e0fea6db7
Питер Паркер <a.shteyman@rsoperation.com>
2023-06-21 06:29:21 +0300

    30 модуль Элементы ФП, выполнено Дополнено 3 задание Добавлена аннтоация в задании 2

--- 30.Элементы функционального программирования/30.2 И снова zip.py
+++ 30.Элементы функционального программирования/30.2 И снова zip.py
@@ -19,6 +19,6 @@ from typing import List
 letters: List[str] = ['a', 'b', 'c', 'd', 'e']
 numbers: List[int] = [1, 2, 3, 4, 5, 6, 7, 8]
 
-results = list(map(lambda x, y: tuple([x, y]), letters, numbers))
+results: List[tuple] = list(map(lambda x, y: tuple([x, y]), letters, numbers))
 print(results)
 
